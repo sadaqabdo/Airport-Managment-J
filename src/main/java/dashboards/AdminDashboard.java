3de3fe5ca1b2bcbec4aa4819fdb2686899b8fb77
elhar <hamza.elharchi.elmaslohi@gmail.com>
2021-12-26 18:22:47 +0100

    Demo crud  operation

---
+++ src/main/java/dashboards/AdminDashboard.java
@@ -0,0 +1,27 @@
+package dashboards;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AdminDashboard extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(AdminDashboard.class.getResource("admin.fxml"));
+            Scene scene = new Scene(root);
+
+            stage.setTitle("Admin");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
